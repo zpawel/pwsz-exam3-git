2701f843b66266dce28ede893262fcd89418654a
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2015-05-03 21:26:23 +0200

    Subtract

--- app/calculator.js
+++ app/calculator.js
@@ -2,5 +2,9 @@ module.exports = {
     add: function (a, b)
     {
         return a + b;
+    },
+    subtract: function (a, b)
+    {
+        return  b - a;
     }
 };
--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -1,12 +1,19 @@
 describe('calculator', function ()
 {
-    var calculator = require('calculator');
+    var calculator = require('../app/calculator.js');
 
     describe('add', function ()
     {
         it('should return sum of two numbers', function ()
         {
             expect(calculator.add(1, 2)).toEqual(3);
+        });
+    });
+    describe('subtract', function ()
+    {
+        it('should return difference between second and first number', function ()
+        {
+            expect(calculator.subtract(1, 2)).toEqual(1);
         });
     });
 });
